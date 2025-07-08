aab500f2d1755158dbc89badb1136a256b0bd8c5
Wesley Evans <162752827+evans-rxfire@users.noreply.github.com>
2025-07-07 19:34:59 -0500

    2025.07.07 Add service worker and manifest.

--- main.js
+++ main.js
@@ -254,3 +254,13 @@ window.addEventListener('DOMContentLoaded', () => {
         htmlElement.classList.add('dark');
     }
 });
+
+
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+        navigator.serviceWorker
+        .register("/service-worker.js")
+        .then((reg) => console.log("Service worker registered:", reg.scope))
+        .catch((err) => console.error("Service worker registration failed:", err));
+    });
+}
